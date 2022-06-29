fa3c135f94b6c961569095dd9049e5538e5d391f
Ivan Vasquez <ivanjv1234@gmail.com>
2022-06-29 00:01:25 -0500

    feat: first steps with routes, layout and header

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Head from 'next/head'
+import Layout from "../components/Layout";
+const Nosotros = () => {
+  return (
+    <Layout
+     pagina='Nosotros'
+    >
+      <h1>Desde nosotros</h1>
+    </Layout>
+  );
+};
+
+export default Nosotros;
