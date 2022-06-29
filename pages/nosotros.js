034456f8074289f64c31cefa5d1bac37167acf29
Ivan Vasquez <ivanjv1234@gmail.com>
2022-06-29 15:28:43 -0500

    feat: adding Nosotros page and styling

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,13 +1,27 @@
 import Layout from "../components/Layout";
-
+import Image from "next/image";
+import styles from '../styles/Nosotros.module.css'
 const Nosotros = () => {
   return (
     <Layout
      pagina='Nosotros'
     >
-      <h1>Desde nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+        <div className={styles.contenido}>
+          <Image layout='responsive' width={600} height={450} src='/img/nosotros.jpg' alt="imagen sobre nosotros" />
+          <div>
+            <p>
+              Praesent bibendum posuere vulputate. Cras felis arcu, consequat ut eros mattis, iaculis sollicitudin elit. Etiam dignissim justo et vulputate eleifend. Nullam varius lectus id dolor varius, lobortis lobortis eros tempor. Integer nec dui gravida, venenatis elit eu, convallis ex. Suspendisse dictum, enim 
+            </p>
+            <p>
+              Praesent bibendum posuere vulputate. Cras felis arcu, consequat ut eros mattis, iaculis sollicitudin elit. Etiam dignissim justo et vulputate eleifend. Nullam varius lectus id dolor varius, lobortis lobortis eros tempor. Integer nec dui gravida, venenatis elit eu, convallis ex. Suspendisse dictum, enim in pellentesque porta, arcu tortor semper nisl, porttitor finibus lectus lectus auctor neque. Proin quam dolor, pharetra nec diam sit amet, gravida mattis sapien. 
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
-  );
+  ); 
 };
 
 export default Nosotros;
