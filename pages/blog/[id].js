34ab9bf51347a1aca07dbd74a2fb58648a221734
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-07 12:55:37 -0500

    feat: Leer el Id de la página actual

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,13 +1,28 @@
 import {useRouter} from 'next/router'
 
-const EntradaBlog = () => {
-  const router = useRouter()
-  console.log(router.query)
+const EntradaBlog = ({data}) => {
+  // const router = useRouter()
+  // console.log(router.query)
+  console.log(data)
   return (
     <div>
-        Desde entrada blog 
+        {data.titulo}
     </div>
   )
+}
+
+export async function getServerSideProps({query: {id} }){
+  // estos console.log solo los podemos ver en la terminal de next
+  const url = `http://localhost:1337/blogs/${id}`
+  const respuesta = await fetch(url)
+  const data = await respuesta.json()
+  // console.log(data)
+
+  return{
+    props: {
+      data
+    }
+  }
 }
 
 export default EntradaBlog
