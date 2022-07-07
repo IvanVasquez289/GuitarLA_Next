3df61df51852f66410da057eb39c58596c663f11
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-06 23:43:02 -0500

    feat: query API

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,26 @@
+// import { useEffect } from "react";
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({data}) => {
+  // no consumiremos la api de esta forma, sino como se indica abajo
+  // useEffect(() => {
+  //   const consultarApi = async ()=>{
+  //     try {
+  //       const url = 'http://localhost:1337/blogs'
+  //       const respuesta = await fetch(url)
+  //       const data = await respuesta.json()
+  //       console.log(data)
+        
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+
+  //   consultarApi()
+  // }, [])
+  
+  console.log(data)
+
   return (
     <Layout
      pagina='Blog'
@@ -9,5 +29,20 @@ const Blog = () => {
     </Layout>
   );
 };
+
+export async function getServerSideProps(){
+
+  const url = 'http://localhost:1337/blogs'
+  const respuesta = await fetch(url)
+  const data = await respuesta.json()
+
+  console.log(data)
+
+  return{
+    props: {
+      data
+    }
+  }
+}
 
 export default Blog;
