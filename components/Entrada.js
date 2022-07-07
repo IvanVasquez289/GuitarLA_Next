0070398c6ef7d85f92ff7ff730be163e24d74047
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-07 01:07:31 -0500

    feat: mostrando las imagenes

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import Image from "next/image"
 import { formatearFecha } from "../helpers"
 const Entrada = ({entrada}) => {
-  const {contenido,resumen,titulo,imagen, published_at, id} = entrada
+  const {resumen,titulo,imagen, published_at, id} = entrada
+
   return (
     <article>
+        <Image width={800} height={600} src={imagen.url} alt={`imagen blog ${titulo}`} />
         <div>
             <h1> {titulo} </h1>
             <p> {formatearFecha(published_at)} </p>
