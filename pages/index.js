a93ef8a033184bc81885eed6bf4a79a64bc99c5e
Ivan Vasquez <ivanjv1234@gmail.com>
2022-07-22 00:38:40 -0500

    feat: creando bloque curso y multiples consultas

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
 import Layout from "../components/Layout";
+import Listado from "../components/Listado";
 
-export default function Home() {
+export default function Home({guitarras, curso}) {
+  console.log(guitarras)
+  console.log(curso)
   return (
     <Layout
       pagina='GuitarLA - Inicio'
     >
-      <h1>Desde inicio</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra coleccion</h1>
+        <Listado
+          guitarras={guitarras}
+        />
+      </main>
     </Layout>
   );
+}
+
+export async function getServerSideProps(){
+
+  // const url = `${process.env.API_URL}/guitarras`
+  // const respuesta = await fetch(url)
+  // const guitarras = await respuesta.json()
+
+  // const url = `${process.env.API_URL}/seccion-cursos`
+  // const respuesta = await fetch(url)
+  // const cursos = await respuesta.json()
+
+  // Podemos hacerlo de una manera donde se llamen simultaneamente
+  const urlGuitarras = `${process.env.API_URL}/guitarras`
+  const urlCursos = `${process.env.API_URL}/seccion-cursos`
+
+  const [resGuitarras, resCursos] =  await Promise.all([ fetch(urlGuitarras), fetch(urlCursos) ])
+  const [guitarras, curso] = await Promise.all([ resGuitarras.json(), resCursos.json() ])
+
+
+
+  console.log(guitarras)
+  return{
+    props: {
+      guitarras,
+      curso
+    }
+  }
 }
